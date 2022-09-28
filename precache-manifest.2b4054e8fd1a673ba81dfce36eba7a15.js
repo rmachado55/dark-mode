c24752765b5dc79cc105fd9f568957f944e88cbc
Ricardo Machado <ricardo.machado7@hotmail.com>
2022-09-27 21:17:24 -0300

    BUILD para GitHub Pages

---
+++ precache-manifest.2b4054e8fd1a673ba81dfce36eba7a15.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e4eaee10d63a449f125ba6412e9307",
+    "url": "/dark-mode/index.html"
+  },
+  {
+    "revision": "82553a9bca5f872287e3",
+    "url": "/dark-mode/static/js/2.9ea3e467.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dark-mode/static/js/2.9ea3e467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3daa417c0686ca277f9c",
+    "url": "/dark-mode/static/js/main.19f05190.chunk.js"
+  },
+  {
+    "revision": "344422e4522963cde8e0",
+    "url": "/dark-mode/static/js/runtime-main.ef9503e6.js"
+  },
+  {
+    "revision": "44dbaa78fb2ed240792c9e7a424b0ea0",
+    "url": "/dark-mode/static/media/alimentacao.44dbaa78.svg"
+  },
+  {
+    "revision": "f3a2bff49642ca68b3841fde422295cb",
+    "url": "/dark-mode/static/media/bank_logo.f3a2bff4.svg"
+  },
+  {
+    "revision": "b97c300bed11302a8687c16e07aa89e5",
+    "url": "/dark-mode/static/media/dinheiro.b97c300b.svg"
+  },
+  {
+    "revision": "acd41ceb3366a71c61b7ed3d5df83682",
+    "url": "/dark-mode/static/media/olho.acd41ceb.svg"
+  },
+  {
+    "revision": "17e95f3cc45dc23ffda7f4d29dc137df",
+    "url": "/dark-mode/static/media/outros.17e95f3c.svg"
+  },
+  {
+    "revision": "134d853a95b2b30676da2c63cda2eeaf",
+    "url": "/dark-mode/static/media/privado.134d853a.svg"
+  },
+  {
+    "revision": "605ef9ca7e70a1bb568ffcec443d05f6",
+    "url": "/dark-mode/static/media/saude.605ef9ca.svg"
+  },
+  {
+    "revision": "0981f5bfd5f197c52e678c3c0616cd81",
+    "url": "/dark-mode/static/media/themeOff.0981f5bf.svg"
+  },
+  {
+    "revision": "75ea28146734a5ea6593dab45d272a90",
+    "url": "/dark-mode/static/media/themeOn.75ea2814.svg"
+  },
+  {
+    "revision": "15a4ed7310630a72e21a8054897c3a51",
+    "url": "/dark-mode/static/media/transporte.15a4ed73.svg"
+  },
+  {
+    "revision": "59035811601fe0a607827eb6408e14ef",
+    "url": "/dark-mode/static/media/utilidades.59035811.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/dark-mode/precache-manifest.2b4054e8fd1a673ba81dfce36eba7a15.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dark-mode/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
